eab3c2316e5bf517d69012af4c669449581c4c35
Saleem Abdulrasool <abdulras@google.com>
2021-07-13 14:24:56 -0700

    build: add curl dependency
    CURL is used for Foundation, and so is part of the dependency chain that
    we need to build before building the toolchain and Foundation.

--- utils/build-windows-toolchain.bat
+++ utils/build-windows-toolchain.bat
@@ -40,7 +40,6 @@ call :CloneDependencies || (exit /b)
 cmake ^
   -B %BuildRoot%\zlib ^
   -D BUILD_SHARED_LIBS=NO ^
-  -D BUILD_TESTING=NO ^
   -D CMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
   -D CMAKE_C_COMPILER=cl ^
   -D CMAKE_INSTALL_PREFIX=%BuildRoot%\Library\zlib-1.2.11\usr ^
@@ -72,6 +71,44 @@ cmake ^
   -S %SourceRoot%\libxml2 || (exit /b)
 cmake --build "%BUildRoot%\libxml2" || (exit /b)
 cmake --build "%BUildRoot%\libxml2" --target install || (exit /b)
+
+:: build curl
+cmake ^
+  -B %BuildRoot%\curl ^
+  -B BUILD_SHARED_LIBS=NO ^
+  -D BUILD_TESTING=NO ^
+  -D CMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
+  -D CMAKE_C_COMPILER=cl ^
+  -D CMAKE_INSTALL_PREFIX=%BuildRoot%\Library\curl-7.77.0\usr ^
+
+  -D BUILD_CURL_EXE=NO ^
+  -D CMAKE_USE_OPENSSL=NO ^
+  -D CURL_CA_PATH=none ^
+  -D CMAKE_USE_SCHANNEL=YES ^
+  -D CMAKE_USE_LIBSSH2=NO ^
+  -D HAVE_POLL_FINE=NO ^
+  -D CURL_DISABLE_LDAP=YES ^
+  -D CURL_DISABLE_LDAPS=YES ^
+  -D CURL_DISABLE_TELNET=YES ^
+  -D CURL_DISABLE_DICT=YES ^
+  -D CURL_DISABLE_FILE=YES ^
+  -D CURL_DISABLE_TFTP=YES ^
+  -D CURL_DISABLE_RTSP=YES ^
+  -D CURL_DISABLE_PROXY=YES ^
+  -D CURL_DISABLE_POP3=YES ^
+  -D CURL_DISABLE_IMAP=YES ^
+  -D CURL_DISABLE_SMTP=YES ^
+  -D CURL_DISABLE_GOPHER=YES ^
+  -D CURL_ZLIB=YES ^
+  -D ENABLE_UNIX_SOCKETS=NO ^
+  -D ENABLE_THREADED_RESOLVER=NO ^
+
+  -D ZLIB_ROOT=%BuildRoot%\Library\zlib-1.2.11\usr ^
+
+  -G Ninja ^
+  -S %SourceRoot%\curl || (exit /b)
+cmake --build "%BuildRoot%\curl" || (exit /b)
+cmake --build "%BuildRoot%\curl" || (exit /b)
 
 :: Clean up the module cache
 rd /s /q %LocalAppData%\clang\ModuleCache
@@ -113,6 +150,7 @@ git clone --quiet --no-tags --depth 1 --branch v1.2.11 https://github.com/madler
 git clone --quiet --no-tags --depth 1 --branch v2.9.12 https://github.com/gnome/libxml2
 git clone --quiet --no-tags --depth 1 --branch version-3.36.0 https://github.com/sqlite/sqlite
 git clone --quiet --no-tags --depth 1 --branch maint/maint-67 https://github.com/unicode-org/icu
+git clone --quiet --no-tags --depth 1 --branch curl-7_77_0 https://github.com/curl/curl
 
 goto :eof
 endlocal
